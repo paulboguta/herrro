b6cd4bbac285999621fb8d52b51b2b762a82e1ca
Pawel Boguta <paul.boguta@gmail.com>
2025-08-12 14:16:41 +0200

    init

---
+++ src/server/db/schema.ts
@@ -0,0 +1,16 @@
+import { sql } from "drizzle-orm";
+import { index, pgTable } from "drizzle-orm/pg-core";
+
+export const posts = pgTable(
+  "post",
+  (d) => ({
+    id: d.integer().primaryKey().generatedByDefaultAsIdentity(),
+    name: d.varchar({ length: 256 }),
+    createdAt: d
+      .timestamp({ withTimezone: true })
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+    updatedAt: d.timestamp({ withTimezone: true }).$onUpdate(() => new Date()),
+  }),
+  (t) => [index("name_idx").on(t.name)],
+);
