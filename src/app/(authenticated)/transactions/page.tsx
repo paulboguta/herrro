b993ba185c947eaac80a1d7fe27c8e6d270580a4
Pawel Boguta <paul.boguta@gmail.com>
2025-08-13 16:38:59 +0200

    add simple toolbar wrapper

--- src/app/(authenticated)/transactions/page.tsx
+++ src/app/(authenticated)/transactions/page.tsx
@@ -1,5 +1,8 @@
+import { Button } from "@/components/ui/button";
 import PageHeader from "@/components/ui/page-header";
+import { Toolbar } from "@/components/ui/toolbar";
 import { api, HydrateClient } from "@/trpc/server";
+import { PlusIcon } from "lucide-react";
 import { Suspense } from "react";
 import TransactionsContents from "./_transactions-contents";
 
@@ -13,9 +16,17 @@ export default function Transactions() {
   return (
     <HydrateClient>
       <PageHeader title="Transactions" />
-      <Suspense fallback={<div>Loading...</div>}>
-        <TransactionsContents />
-      </Suspense>
+      <div className="space-y-4">
+        <Toolbar>
+          <Button variant="outline" size="sm">
+            <PlusIcon />
+            Add
+          </Button>
+        </Toolbar>
+        <Suspense fallback={<div>Loading...</div>}>
+          <TransactionsContents />
+        </Suspense>
+      </div>
     </HydrateClient>
   );
 }
---
+++ src/components/ui/toolbar.tsx
@@ -0,0 +1,9 @@
+function Toolbar({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex items-center gap-2">
+      {children}
+    </div>
+  );
+}
+
+export { Toolbar };
